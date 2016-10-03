7ef20e00287579c9982974fb8d5b2bc2c7b523b9
jrock78 <jnatoli5@gmail.com>
2016-10-03 12:45:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,16 @@ var config = {};
 
 config.bots = [
   {
-    botID: 'xxxxxxxxxx',
-    groupID: 'xxxxxxxx',
+    botID: '8be9eb9671e297e918f2270c14',
+    groupID: '21875253',
     groupLocalID: '1', // an identifier for this group used in this application only
-    botName: 'mybot',
-    groupName: 'the-group-name' // optional
+    botName: 'Captain_Faggot',
   }
 ];
 
 config.yahooWeatherAppId = '';
 
 // Travis CI user token
-config.travisUserToken = 'xxxxxxxxxxxxxx';
+config.JonUserToken = 'FQfgHgUc1YQkOlydPQ457sbkqhJ4u69Bnja0BOXn';
 
 module.exports = config;
